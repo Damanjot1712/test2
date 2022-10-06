39088b5f12861088ebe39a727492c2c5129a22f3
Damanjot Singh <damanjotsingh1712@gmail.com>
2022-10-06 14:38:16 -0400

    update server.js

--- server.js
+++ server.js
@@ -6,22 +6,14 @@ onHttpStart = ()=>{
     console.log('Express http server listening on port ' + HTTP_PORT);
 }
 app.use(express.static('public'));
-
-app.get("/", function(req, res){
-    let resText = "<h2>Declaration</h2> <br> <br>";
-    resText += "The rest text is diplayed in the paragraph as shown in the screenshot.<br><br>"; 
-    resText += "I acknowledge the College's academic integrity policy - and my own integrity - remain in effect whether my work is done remotely or onsite. Any test or assingnment is in act of trust between me and my instructor, and specially with my classmates... even when no one is watching. I Declare I will not break that trust.<br><br>"; 
-    resText += "Name: Damanjot Singh<br><br>";
-    resText += "Student Number: 148285216<br><br>"; 
-    resText += "<a href = './students.json'> Click to visit CPA Students </a> <br>";
-    
-
+app.get("/", function(req,res){
+    res.send("<h2>Declaration</h2>"+
+    "<p>The rest text is diplayed in the paragraph as shown in the screenshot.<br> I acknowledge the College's academic integrity policy - and my own integrity - remain in effect whether my work is done remotely or onsite. Any test or assingnment is in act of trust between me and my instructor, and specially with my classmates... even when no one is watching. I Declare I will not break that trust.<br/>Name: Damanjot Singh<br />Student Number: 148285216<br /><a href='/cpa'>Click to visit CPA Students.</a><br><a href='/cpa'>Click to see who has the highest GPA.</a></p>");
 });
-
-
-
-
-
+app.get("/data_prep.js", function(req,res){
+    res.sendFile(path.join(__dirname,"/data_prep.js"));
+});
+app.listen(HTTP_PORT, onHttpStart);
 
 
 app.use((req, res)=>{
